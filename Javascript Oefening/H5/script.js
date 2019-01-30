10ba4b86af33de3a7b600762beeb9439a30966e5
Ahmed Benhajar <302267915@student.rocmondriaan.nl>
2019-01-30 12:57:24 +0100

    Veder aan H5 gewerkt

--- Javascript Oefening/H5/script.js
+++ Javascript Oefening/H5/script.js
@@ -1,18 +1,34 @@
 whileLus();
+getDocument();
+forLus();
+
 function whileLus(){
-    var teller=0;
-    while(teller<=10){
-        var tekst= teller+" X "+10+" = "+parseInt(teller)*parseInt(10)+'<br />';
-         document.getElementById('whileLus').innerHTML+=tekst;
-         teller++;
+    var teller=10;
+    while(teller>=0){
+        var tekst= teller+" x "+10+" = "+parseInt(teller)*parseInt(10)+'<br />';
+         document.getElementById('whileLusSom').innerHTML+=tekst;
+         teller--;
     }
-    navigator();
-function navigator(){
+}
+  
+function forLus(){
+    for(var i=10;i>=0;i--){
+         document.getElementById('forLusSom').innerHTML+= i+" x "+10+" = "+parseInt(i)*parseInt(10)+'<br />';
+    }
+}
+  
+function getDocument(){
+    for(var i in window.document){
+        for( var i=0;i<10;i++){
+                document.getElementById('nav').innerHTML+= 
+                i +': ' + window.document[i] +'<br />'; 
+        }
+   }
+}
+
+function getNavigator(){
     for( var i in window.navigator){
         document.getElementById('nav').innerHTML+= 
             i +': ' + window.navigator[i] +'<br />';
-    }
-   
-}
-   
+    }  
 }
