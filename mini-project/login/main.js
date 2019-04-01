1eb85b45403dd0636a43992379ad9e98f060a938
Ahmed Benhajar <302267915@student.rocmondriaan.nl>
2019-04-01 12:54:19 +0200

    veder aan login gewerkt

--- mini-project/login/main.js
+++ mini-project/login/main.js
@@ -11,33 +11,47 @@ window.onload = function (){
         },
 
         {
-            username: "kip",
-            password: "0000"  
+            username: "1",
+            password: "2"  
         }
     ]
-
-    document.getElementById("submit").addEventListener("click", function(){
+        checkUsername= false;
+        checkPassword= false;
+        document.getElementById("submit").addEventListener("click", function(){
         var username = document.getElementById("username").value;
         var password = document.getElementById("password").value;
         for(var i=0; i<loginData.length; i++){
-            if(username == loginData[i].username && password == loginData[i].password){
-                console.log("login is successfully");
-                document.getElementById("username").style.border=" 1px solid black";
-                document.getElementById("password").style.border=" 1px solid black";
-                return
-            }
-        }
-        for(var i=0; i<loginData.length; i++){
-            if(username != loginData[i].username){
-                console.log("Wrong Username");
+            if(username == ""){
                 document.getElementById("username").style.border=" 1px solid red";
-                return  
             }
-    
-            if(password != loginData[i].password){
+
+            if(password == ""){
+                document.getElementById("password").style.border=" 1px solid red";
+            }
+
+            if(username == loginData[i].username){
+                var checkUsername=true;
+                document.getElementById("username").style.border=" 1px solid black";
+            }
+
+            else{
+                console.log("Wrong Username");
+                document.getElementById("username").style.border=" 1px solid red";  
+            }
+
+            if(password == loginData[i].password){
+                checkPassword= true;
+                document.getElementById("password").style.border=" 1px solid black";
+            }
+
+            else{
                 console.log("Wrong password");  
                 document.getElementById("password").style.border=" 1px solid red";
+            }
 
+            if(checkPassword&&checkUsername){
+                console.log("login is succesfuly!")
+                document.getElementById("message").style.display="block";
                 return
             }
         }
